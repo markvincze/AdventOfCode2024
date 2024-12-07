77454937f32ebfe069599833c378f968a1e5948b
Mark Vincze <mrk.vincze@gmail.com>
2024-12-07 19:37:12 +0100

    Day 7 part 2

--- FSharp/07-bridge-repair.fsx
+++ FSharp/07-bridge-repair.fsx
@@ -22,3 +22,22 @@ let equations = File.ReadAllLines "FSharp/07-bridge-repair-input.txt"
 let result1 = equations
               |> List.filter (fun (result, operands) -> isPossible result operands)
               |> List.sumBy fst
+
+let concatenate a b =
+    let digitCount = floor((log10 (float b)) + 1.0) |> int
+    a * (pown 10L digitCount) + b
+
+let isPossible2 result operands =
+    let rec isPossible result operands acc =
+        if acc > result then false
+        else match operands with
+             | [] -> acc = result
+             | h :: t -> isPossible result t (acc + h) ||
+                         isPossible result t (acc * h) ||
+                         isPossible result t (concatenate acc h)
+
+    isPossible result (List.skip 1 operands) (List.head operands)
+
+let result2 = equations
+              |> List.filter (fun (result, operands) -> isPossible2 result operands)
+              |> List.sumBy fst
