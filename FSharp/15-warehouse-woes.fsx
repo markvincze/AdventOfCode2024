8672c83b3e3ebb41424305ad5ae3e61863c6159b
Mark Vincze <mrk.vincze@gmail.com>
2024-12-17 11:38:11 +0100

    Day 15 part 2

--- FSharp/15-warehouse-woes.fsx
+++ FSharp/15-warehouse-woes.fsx
@@ -70,22 +70,20 @@ let move dir (x, y) (tiles : Tile[,]) =
              else (x, y)
 
 let rec executeSteps tiles (rx, ry) steps =
-    // print tiles (rx, ry)
     match steps with
     | [] -> (rx, ry)
     | h :: t -> let nx, ny = move h (rx, ry) tiles
                 executeSteps tiles (nx, ny) t
 
-// let fx, fy = executeSteps tiles (rx, ry) steps
+let fx, fy = executeSteps tiles (rx, ry) steps
 
-// // print tiles (fx, fy)
 
-// let result1 = seq { for x in 0..Array2D.length1 tiles - 1 do
-//                         for y in 0..Array2D.length2 tiles - 1 do
-//                             (x, y) }
-//               |> Seq.sumBy (fun (x, y) -> match tiles[x, y] with
-//                                           | Box -> y * 100 + x
-//                                           | _ -> 0)
+let result1 = seq { for x in 0..Array2D.length1 tiles - 1 do
+                        for y in 0..Array2D.length2 tiles - 1 do
+                            (x, y) }
+              |> Seq.sumBy (fun (x, y) -> match tiles[x, y] with
+                                          | Box -> y * 100 + x
+                                          | _ -> 0)
 
 // Part 2
 
@@ -101,79 +99,54 @@ let tiles2 = Array2D.init
 
 let (rx2, ry2) = (rx * 2, ry)
 
-// print tiles2 (rx2, ry2)
-
-let rec canPushBoxes dir (x, y) (tiles : Tile [,]) =
-    // printfn "canPushBoxes called"
-    match tiles[x, y] with
-    | Empty -> true
-    | Wall -> false
-    | _ when dir = Left || dir = Right -> canPushBoxes dir ((x, y) |> step dir |> step dir) tiles
+let rec canPushBoxes dir (x, y) (tiles : Tile [,]) pushedBoxes =
+    match tiles[x, y], dir with
+    | Empty, _ -> true, pushedBoxes
+    | Wall, _ -> false, Set.empty<int * int>
+    | _, Left | _, Right -> let pushedBoxes = match tiles[x, y] with
+                                              | BoxLeft -> Set.add (x, y) pushedBoxes
+                                              | BoxRight -> Set.add (x - 1, y) pushedBoxes
+                                              | _ -> pushedBoxes
+                            canPushBoxes dir ((x, y) |> step dir |> step dir) tiles pushedBoxes
     | _ -> let nx, ny = step dir (x, y)
            let nx2, ny2 = match tiles[x, y] with
                           | BoxLeft -> (nx + 1, ny)
                           | BoxRight -> (nx - 1, ny)
                           | _ -> failwith "Invalid input"
-           canPushBoxes dir (nx, ny) tiles &&
-           canPushBoxes dir (nx2, ny2) tiles
+           let pushedBoxes = match tiles[x, y] with
+                             | BoxLeft -> Set.add (x, y) pushedBoxes
+                             | BoxRight -> Set.add (x - 1, y) pushedBoxes
+                             | _ -> pushedBoxes
+           match canPushBoxes dir (nx, ny) tiles pushedBoxes with
+           | true, pushedBoxes -> canPushBoxes dir (nx2, ny2) tiles pushedBoxes
+           | false, _ -> false, Set.empty<int * int>
 
-let pushBoxes2 dir (x, y) (tiles : Tile [,]) =
-    printfn "pushBoxes2 called"
-    let rec pushBoxes2 dir (x, y) prev (tiles : Tile [,]) isSecondPush =
-        let prevN = tiles[x, y]
-        match dir with
-        | Left | Right -> match prevN with
-                          | BoxLeft | BoxRight -> tiles[x, y] <- prev
-                                                  pushBoxes2 dir (step dir (x, y)) prevN tiles isSecondPush
-                          | Empty -> tiles[x, y] <- prev
-        | Up | Down -> match prevN with
-                       | BoxLeft | BoxRight -> tiles[x, y] <- prev
-                                               let nx, ny = step dir (x, y)
-                                               let nx2, ny2 = match prevN with
-                                                              | BoxLeft -> (nx + 1, ny)
-                                                              | BoxRight -> (nx - 1, ny)
-                                                              | _ -> failwith "Invalid input 1"
-                                               pushBoxes2 dir (nx, ny) prevN tiles isSecondPush
-                                               if tiles[nx, y] <> Empty && not isSecondPush
-                                               then pushBoxes2 dir (nx2, ny2) Empty tiles true
-                       | Empty -> tiles[x, y] <- prev
-
-    match dir with
-    | Left | Right -> pushBoxes2 dir (x, y) Empty tiles false
-    | Up | Down -> let nx, ny = match tiles[x, y] with
-                                | BoxLeft -> x + 1, y
-                                | BoxRight -> x - 1, y
-                                | _ -> failwith "Invalid input 2"
-                //    printfn "Before first pushBoxes2"
-                //    print tiles (rx, ry)
-                   pushBoxes2 dir (x, y) Empty tiles false
-                //    printfn "After first pushBoxes2"
-                //    print tiles (rx, ry)
-                   pushBoxes2 dir (nx, ny) Empty tiles true
-                //    printfn "After second pushBoxes2"
-                //    print tiles (rx, ry)
+let pushBoxes3 dir pushedBoxes (tiles : Tile [,]) =
+    pushedBoxes |> Seq.iter (fun (x, y) -> tiles[x, y] <- Empty
+                                           tiles[x + 1, y] <- Empty)
+    
+    pushedBoxes
+    |> Seq.iter (fun (x, y) -> let nx, ny = step dir (x, y)
+                               tiles[nx, ny] <- BoxLeft
+                               tiles[nx + 1, ny] <- BoxRight)
 
 let move2 dir (x, y) (tiles : Tile[,]) =
-    printfn "move2 called"
     let nx, ny = step dir (x, y)
     match tiles[nx, ny] with
     | Empty -> (nx, ny)
     | Wall -> (x, y)
-    | BoxLeft | BoxRight -> if canPushBoxes dir (nx, ny) tiles
-                            then pushBoxes2 dir (nx, ny) tiles
-                                 (nx, ny)
-                            else (x, y)
+    | BoxLeft | BoxRight -> match canPushBoxes dir (nx, ny) tiles Set.empty<int * int> with
+                            | true, pushedBoxes -> pushBoxes3 dir pushedBoxes tiles
+                                                   (nx, ny)
+                            | false, _ -> (x, y)
 
 let rec executeSteps2 tiles (rx, ry) steps =
     match steps with
     | [] -> (rx, ry)
-    | h :: t -> printfn $"executeStep2 called. Step {h}"
-                print tiles (rx, ry)
-                let nx, ny = move2 h (rx, ry) tiles
+    | h :: t -> let nx, ny = move2 h (rx, ry) tiles
                 executeSteps2 tiles (nx, ny) t
 
-let fx2, fy2 = executeSteps2 tiles2 (rx2, ry2) (steps |> List.take 340)
-// let fx2, fy2 = executeSteps2 tiles2 (rx2, ry2) steps
+let fx2, fy2 = executeSteps2 tiles2 (rx2, ry2) steps
 
 // print tiles (fx, fy)
 
