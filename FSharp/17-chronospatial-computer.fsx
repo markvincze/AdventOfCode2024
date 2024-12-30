f3888b07e70d669bff032c07da198d9f54f14ab4
Mark Vincze <mrk.vincze@gmail.com>
2024-12-30 13:14:27 +0100

    Day 17

--- FSharp/17-chronospatial-computer.fsx
+++ FSharp/17-chronospatial-computer.fsx
@@ -32,22 +32,58 @@ let combo state = function
                   | 6 -> state.c
                   | _ -> failwith "Invalid input"
 
-let processOp (opCode, operand) state output =
+let processOp (opCode, operand) state =
     match opCode with
-    | 0 -> { state with a = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, output
-    | 1 -> { state with b = state.b ^^^ operand; ip = state.ip + 2 }, output
-    | 2 -> { state with b = (combo state operand) % 8L; ip = state.ip + 2 }, output
-    | 3 -> { state with ip = if state.a = 0 then state.ip + 2 else operand }, output
-    | 4 -> { state with b = state.b ^^^ state.c; ip = state.ip + 2 }, output
-    | 5 -> { state with ip = state.ip + 2 }, (combo state operand % 8L) :: output
-    | 6 -> { state with b = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, output
-    | 7 -> { state with c = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, output
+    | 0 -> { state with a = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, None
+    | 1 -> { state with b = state.b ^^^ operand; ip = state.ip + 2 }, None
+    | 2 -> { state with b = (combo state operand) % 8L; ip = state.ip + 2 }, None
+    | 3 -> { state with ip = if state.a = 0 then state.ip + 2 else operand }, None
+    | 4 -> { state with b = state.b ^^^ state.c; ip = state.ip + 2 }, None
+    | 5 -> { state with ip = state.ip + 2 }, Some (combo state operand % 8L)
+    | 6 -> { state with b = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, None
+    | 7 -> { state with c = state.a / (pown 2L (combo state operand |> int)); ip = state.ip + 2 }, None
     | _ -> failwith "Invalid input"
 
 let rec runProgram state (program : int[]) output =
     if state.ip < 0 || state.ip > program.Length - 2
-    then String.Join(',', output |> List.rev)
-    else let state, output = processOp (program[state.ip], program[state.ip + 1]) state output
+    then output
+    else let state, newOutput = processOp (program[state.ip], program[state.ip + 1]) state
+         let output = match newOutput with
+                      | None -> output
+                      | Some n -> n :: output
          runProgram state program output
 
-let result1 = runProgram state program []
+let result1 = String.Join(',', runProgram state program [] |> List.rev)
+
+let rec runProgram2 state (program : int[]) outputCnt =
+    if state.ip < 0 || state.ip > program.Length - 2
+    then outputCnt = program.Length
+    // else if List.length output > maxLength
+    // then None
+    else let state, newOutput = processOp (program[state.ip], program[state.ip + 1]) state
+         match newOutput with
+         | None -> runProgram2 state program outputCnt
+         | Some n when outputCnt > program.Length - 1 -> false
+         | Some n when n <> program[outputCnt] -> false
+         | _ -> runProgram2 state program (outputCnt + 1)
+
+// let expectedOutput = lines[4].Split(": ")[1]
+
+let rec findResult2 state (program : int[]) =
+    if runProgram2 state program 0
+    then state.a
+    else findResult2 { state with a = state.a + 1L } program
+    // match runProgram state program [] maxLength with
+    // | None -> findResult2 { state with a = state.a + 1L } program programRev maxLength
+    // | Some o when o = programRev -> state.a
+    // | _ -> findResult2 { state with a = state.a + 1L } program programRev maxLength
+
+// let result2 = findResult2 state program
+
+// let rec runProgram state (program : int[]) output maxLength =
+//     if state.ip < 0 || state.ip > program.Length - 2
+//     then Some output
+//     else if List.length output > maxLength
+//     then None
+//     else let state, output = processOp (program[state.ip], program[state.ip + 1]) state output
+//          runProgram state program output maxLength
---
+++ FSharp/Projects/Day17/Program.cs
@@ -0,0 +1,167 @@
+﻿//int[] expectedOutput = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 1, 5, 5, 3, 0];
+//2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 1, 5, 5, 3, 0
+//5, 2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 1, 5, 5, 3, 0
+//Console.WriteLine("Length: {0}", expectedOutput.Length);
+
+//TestSol();
+
+//return;
+
+var result = FindSol();
+
+Console.WriteLine("Result: {0}", result);
+return;
+// Last attempt: 530250000000
+// Last attempt: 603680000000
+// Last attempt: 35184372088832
+
+IEnumerable<long> Range(long start, int count)
+{
+    for (long i = start; i < start + count; i++)
+    {
+        yield return i;
+    }
+}
+
+int[] expectedOutput = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 1, 5, 5, 3, 0];
+
+var options = new List<long> { 0 };
+List<long> prevOptions = null;
+
+foreach (var output in expectedOutput.Reverse())
+{
+    prevOptions = options;
+    Console.WriteLine("Finding solutions for output {0}, option count: {1}", output, options.Count);
+    options = options
+        .SelectMany(opt => Range(opt, 8).Where(o => CalcOutput(o) == output))
+        .Select(opt => opt * 8)
+        .ToList();
+}
+
+foreach (var opt in prevOptions)
+{
+    Console.WriteLine(opt);
+}
+return;
+foreach (var a in Enumerable.Range(0, 8))
+{
+    Console.WriteLine("A: {0}, Output: {1}", a, CalcOutput(a));
+}
+
+foreach (var a in Enumerable.Range(24, 8))
+{
+    Console.WriteLine("A: {0}, Output: {1}", a, CalcOutput(a));
+}
+
+foreach (var a in Enumerable.Range(192, 8))
+{
+    Console.WriteLine("A: {0}, Output: {1}", a, CalcOutput(a));
+}
+
+long CalcOutput(long a)
+{
+    var b = 0L;
+    var c = 0L;
+
+    b = a % 8; // 2,4
+    b = b ^ 5; // 1,5, XOR 00000101. 0 -> 5, 1 -> 4, 2 -> 7, 3 -> 6, 4 -> 1, 5 -> 0, 6 -> 3, 7 -> 2
+    c = a / (long)Math.Pow(2, b); // 7,5
+    b = b ^ 6; // 1,6, XOR 00000110
+    a = a / 8; // 0,3
+    b = b ^ c; // 4,1
+
+    return b % 8;
+}
+
+long FindSol()
+{
+    int[] expectedOutput = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 1, 5, 5, 3, 0];
+    //var aInit = 35184372088832L;
+    //var aInit = 847849244544200L;
+    var aInit = 105981155568020L;
+
+    while (true)
+    {
+        if (aInit % 10000000 == 0)
+        {
+            Console.WriteLine("Trying with {0}", aInit);
+        }
+
+        var a = aInit;
+        var b = 0L;
+        var c = 0L;
+
+        var outCnt = 0;
+
+        while (true)
+        {
+            b = a % 8; // 2,4
+            b = b ^ 5; // 1,5, XOR 00000101. 0 -> 5, 1 -> 4, 2 -> 7, 3 -> 6, 4 -> 1, 5 -> 0, 6 -> 3, 7 -> 2
+            c = a / (long)Math.Pow(2, b); // 7,5
+            b = b ^ 6; // 1,6, XOR 00000110
+            a = a / 8; // 0,3
+            b = b ^ c; // 4,1
+
+            var o = b % 8;
+
+            // 
+
+            if (outCnt > expectedOutput.Length - 1)
+            {
+                //Console.WriteLine("Produced too much output");
+                break;
+            }
+            else if (o != expectedOutput[outCnt])
+            {
+                //Console.WriteLine("Output did not match");
+                break;
+            }
+
+            outCnt++;
+
+            if (a == 0)
+            {
+                if (outCnt == expectedOutput.Length)
+                {
+                    return aInit;
+                }
+                else
+                {
+                    //Console.WriteLine("Produced not enought output");
+                    break;
+                }
+            }
+        }
+
+        aInit++;
+    }
+}
+
+void TestSol()
+{
+    var a = 847849244544208L;
+    var b = 0L;
+    var c = 0L;
+
+    var output = new List<long>();
+
+    while (true)
+    {
+        b = a % 8; // 2,4
+        b = b ^ 5; // 1,5
+        c = a / (long)Math.Pow(2, b); // 7,5
+        b = b ^ 6; // 1,6
+        a = a / 8; // 0,3
+        b = b ^ c; // 4,1
+
+        var o = b % 8;
+
+        output.Add(o);
+
+        if (a == 0)
+        {
+            Console.WriteLine("Result: {0}", String.Join(",", output));
+            return;
+        }
+    }
+}
