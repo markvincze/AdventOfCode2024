2b8f2eac76fa83a99c8a22775e4b5a53d425ada9
Mark Vincze <mrk.vincze@gmail.com>
2024-12-30 16:39:14 +0100

    Day 19 part 1

--- FSharp/19-linen-layout.fsx
+++ FSharp/19-linen-layout.fsx
@@ -3,30 +3,41 @@ open System.IO
 
 let lines = File.ReadAllLines "FSharp/19-linen-layout-input.txt"
 
-let towels = lines[0].Split ", "
+let towels = lines[0].Split ", " |> List.ofArray
 let designs = lines |> Array.skip 2
 
+let isPossible (towels : string list) (design : string) =
+    let isMatch (str : string) from (sub : string) =
+        if from + sub.Length <= str.Length
+        then seq { 0..sub.Length - 1 }
+            |> Seq.forall (fun i -> str[from + i] = sub[i])
+        else false
 
-let isPossible (towels : string[]) (design : string) =
-    let rec findSolution from (towels : string[]) (design : string) cache =
-    let rec isPossible from (towels : string[]) (design : string) cache =
-        if from < 30 then printfn $"isPossible called, from: {from}"
-        let isMatch (str : string) from (sub : string) =
-            if from + sub.Length <= str.Length
-            then seq { 0..sub.Length - 1 }
-                |> Seq.forall (fun i -> str[from + i] = sub[i])
-            else false
-
-        if from = design.Length
-        then true
-        else towels
-             |> Array.exists (fun t -> isMatch design from t && isPossible (from + t.Length) towels design cache)
+    let rec findSolution from (allTowels : string list) (towels : string list) (design : string) notPossible =
+        // printfn $"findSolution called, from: {from}"
+        match towels with
+        | [] -> false, Set.add from notPossible
+        | h :: t -> //printfn $"Testing {h}"
+                    if isMatch design from h
+                    then let result, notPossible = isPossible (from + h.Length) allTowels design notPossible
+                         if result
+                         then true, notPossible
+                         else findSolution from allTowels t design notPossible
+                    else findSolution from allTowels t design notPossible
+    and isPossible from (towels : string list) (design : string) notPossible =
+        // printfn $"isPossible called, from: {from}"
+        // (false, notPossible)
+        if (Set.contains from notPossible) then (false, notPossible)
+        else if from = design.Length then (true, notPossible)
+        else findSolution from towels towels design notPossible
+            // towels
+            //  |> Array.exists (fun t -> isMatch design from t && isPossible (from + t.Length) towels design cache)
             //  |> Array.filter (fun t -> isMatch design from t)
             //  |> Array.exists (fun t -> isPossible (from + t.Length) towels design)
 
-    isPossible 0 towels design
+    isPossible 0 towels design Set.empty<int> |> fst
 
 let result1 = designs
-              |> Array.take 1
-              |> Array.filter (isPossible 0 towels)
-            //   |> Array.length
+            //   |> Array.take 1
+              |> Array.filter (isPossible towels)
+              |> Array.length
