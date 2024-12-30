1b404d9eb81342b03684ff1fefeaa283d8a1babf
Mark Vincze <mrk.vincze@gmail.com>
2024-12-30 14:14:58 +0100

    Day 18

--- FSharp/18-ram-run.fsx
+++ FSharp/18-ram-run.fsx
@@ -7,12 +7,13 @@ let byteCount = 1024
 let width = 71
 let height = 71
 
-let corrupted = lines
-                     |> Array.take byteCount
-                     |> Array.map
-                            (fun s -> let [|x; y|] = s.Split ','
-                                      (Int32.Parse x, Int32.Parse y))
-                     |> Set.ofArray
+let getCorruptedN n (lines : string[]) =
+    lines
+    |> Array.take n
+    |> Array.map
+           (fun s -> let [|x; y|] = s.Split ','
+                     (Int32.Parse x, Int32.Parse y))
+    |> Set.ofArray
 
 let possibleNextSteps w h visited corrupted (x, y) =
     let canStep (x, y) = not <| Set.contains (x, y) visited && not <| Set.contains (x, y) corrupted
@@ -24,11 +25,15 @@ let possibleNextSteps w h visited corrupted (x, y) =
     }
 
 let rec findShortestPath w h q visited corrupted stepCount =
-    if q |> Set.contains (w - 1, h - 1)
-    then stepCount
+    if Set.isEmpty q then None
+    else if q |> Set.contains (w - 1, h - 1) then Some stepCount
     else let newQueue = q
                         |> Seq.collect (possibleNextSteps w h visited corrupted)
                         |> Set.ofSeq
          findShortestPath w h newQueue (Set.union visited q) corrupted (stepCount + 1)
 
-let result1 = findShortestPath width height (Set.singleton (0, 0)) Set.empty<int * int> corrupted 0
+let result1 = findShortestPath width height (Set.singleton (0, 0)) Set.empty<int * int> (getCorruptedN 1024 lines) 0
+
+// let result2 =
+//     seq { 1024..lines.Length }
+//     |> Seq.find (fun n -> findShortestPath width height (Set.singleton (0, 0)) Set.empty<int * int> (getCorruptedN n lines) 0 = None)
